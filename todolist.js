4430756d8dfb6c97a880a8750dd0567e4608bbe6
eldehd9898 <eldehd9898@gmail.com>
2019-05-08 23:09:26 +0900

    [ADD]Local Storage function

--- todolist.js
+++ todolist.js
@@ -2,16 +2,27 @@
     "use strict"
 
     const qs = select => document.querySelector(select);
-    let listMap = new Map();    
     let listCount = 0;
     const BTN_KIND_SAVE = "save";
     const BTN_KIND_EDIT = "edit";
+    let listData = [];
+    let data = {
+        id : "",
+        value : "",
+        btnKind : "",
+        check : false
+    };
+    let totalListHtml = "";
 
     function todoListApp(){
 
         function init(){
+            rendering();
             enterkey();
             listClickEvent();
+            entireCheck();
+            entireCheckClear();
+            entireDelete();
         }
 
         // EnterKey
@@ -22,7 +33,10 @@
                     if(textValue === "")
                         alert("내용을 입력하시오.");
                     else{
-                        mapSetting(textValue,String(listCount));
+                        data.id = listCount;
+                        data.value = textValue;
+                        data.btnKind = BTN_KIND_EDIT;
+                        listData.push({...data});
                         listCount ++;
                         qs("#input-text").value="";
                         rendering();
@@ -32,36 +46,60 @@
             
 
         }
-        //map stting
-        function mapSetting(value,listId){
-            let liInnerHtml = "<li class='list-unit'>"+addCheckbox(value,listId)+addDelEditButton("edit",listId)+"</li>";
-            listMap.set(listId,liInnerHtml);
+        
+        function liInnerCheckBox(value,listId,btnKind,checkedState){
+            let liInnerHtml = "<li class='list-unit'>"+addCheckbox(value,listId,checkedState)+addDelEditButton(btnKind,listId)+"</li>";
+            return liInnerHtml;
+        }
+
+        function liInnerCheckBoxText(value,listId,btnKind,checkedState){
+            let liInnerHtml = "<li class='list-unit'>"+addCheckBoxAndText(value,listId,checkedState)+addDelEditButton(btnKind,listId)+"</li>";
+            return liInnerHtml;
+        }
+
+        function listHtmlCombine(){
+            totalListHtml = "";
+            for( let i = 0 ; i < listData.length ; i++ ){
+                if (listData[i].btnKind === BTN_KIND_SAVE ){
+                    totalListHtml +=liInnerCheckBoxText(listData[i].value, listData[i].id, listData[i].btnKind, listData[i].check );
+                    continue;
+                }
+                totalListHtml += liInnerCheckBox(listData[i].value, listData[i].id, listData[i].btnKind, listData[i].check );
+
+            }
+            return totalListHtml;
         }
         //list refresh
         function rendering(){
-            let totalList = "";
-            for( let value of listMap.values()){
-                totalList += value;
+            localStorageSetting();
+            listHtmlCombine();
+            qs("#full-list").innerHTML = totalListHtml;
+        }
+
+        function localStorageSetting(){
+            if(listData.length <= 0 && localStorage.getItem("data") === "null"){
+                return;
+            } else if ( listData.length <= 0 ){
+                listData = JSON.parse(localStorage.getItem("data"));
+            } else { 
+                localStorage.setItem("data",JSON.stringify(listData));
+                listData = JSON.parse(localStorage.getItem("data"));
             }
-            qs("#full-list").innerHTML = totalList;
+
         }
+
         //Add checkbox add
-        function addCheckbox(value,chkId){
+        function addCheckbox(value,chkId,checkedState){
             return `<label class="check-label" >
-            <input type="checkbox" name="checkBoxList" class="chk" id=${chkId}>
-            <span class="checkbox-circle">${value}</span></label>`;
-        }
-        function addCheckedbox(value,chkId){
-            return `<label class="check-label" >
-            <input type="checkbox" name="checkBoxList" class="chk" id=${chkId} checked>
-            <span class="checkbox-circle">${value}</span></label>`;
+            <input type="checkbox" name="checkBoxList" class="chk" ${checkedState === true ? "checked" : ""}>
+            <span class="checkbox-circle" id=${chkId}>${value}</span></label>`;
         }
 
         //Add checkbox and input text
         function addCheckBoxAndText(text,chkId){
             return `<label class="check-label">
-            <input type="checkbox" name="checkBoxList" id=${chkId}>
-            <span class="checkbox-circle"></span>
+            <input type="checkbox" name="checkBoxList" >
+            <span class="checkbox-circle" id=${chkId}></span>
             </label> 
             <input type="text" id="input-checkbox-text" value=${text} autofocus>`;
 
@@ -78,32 +116,57 @@
             ulItem.addEventListener("click",e=>{
                 let targetId = idSearch(e.target.id);
                 if (e.target.id === `del${targetId}`){
-                    listMap.delete(targetId);
+                    listData.splice(idFindIndex(targetId),1);
                     rendering();
                 } else if (e.target.id === `edit${targetId}`){
-                    let preText = e.target.parentNode.parentNode.children[0].children[1].textContent;
-                    listMap.set(targetId,`<li class='list-unit'>${addCheckBoxAndText(preText,targetId)}
-                    ${addDelEditButton(BTN_KIND_SAVE,targetId)}</li>`);
+                    listData[idFindIndex(targetId)].btnKind = BTN_KIND_SAVE;
                     rendering();
                 } else if (e.target.id === `save${targetId}`){
                     let editText = qs("#input-checkbox-text").value;
-                    listMap.set(targetId,`<li class='list-unit'>${addCheckbox(editText,targetId)}
-                    ${addDelEditButton(BTN_KIND_EDIT,targetId)}</li>`);
+                    listData[idFindIndex(targetId)].value = editText;
+                    listData[idFindIndex(targetId)].btnKind = BTN_KIND_EDIT;
                     rendering();
-                }  else if (e.target.parentNode.className ==="check-label"){
-                    let textValue = e.target.parentNode.children[1].textContent;
-                    let chkItem = document.getElementById(targetId);
-                    if(chkItem.checked){
-                        listMap.set(targetId,`<li class='list-unit'>${addCheckedbox(textValue,targetId)}
-                        ${addDelEditButton(BTN_KIND_EDIT,targetId)}</li>`);
-                    } else{
-                        listMap.set(targetId,`<li class='list-unit'>${addCheckbox(textValue,targetId)}
-                        ${addDelEditButton(BTN_KIND_EDIT,targetId)}</li>`);
-                    }
-                
+                }  
+                else if (e.target.parentNode.children[0].className ==="chk"){
+                    let index = idFindIndex(targetId);
+                    listData[index].check === true ? listData[index].check = false : listData[index].check = true;
                 }
+
             });
         }
+
+        function entireCheck(){
+            qs("#entire-checked").addEventListener("click",e=>{
+                listData.forEach(item=>{
+                    item.check = true;
+                });
+                rendering();
+            });
+        }
+        function entireCheckClear(){
+            qs("#entire-checked-clear").addEventListener("click",e=>{
+                listData.forEach(item=>{
+                    item.check = false;
+                });
+                rendering();
+            });
+        }
+        function entireDelete(){
+            qs("#entire-delete").addEventListener("click",e=>{
+                listData = [];
+                rendering();
+            });
+        }
+
+        function idFindIndex(id){
+            let Idindex; 
+            listData.forEach((item,index)=>{
+                if(item.id === Number(id)){
+                    Idindex = index;
+                }
+            });
+            return Idindex;
+        }  
 
         //buttonId search
         function idSearch(btnId){
@@ -115,6 +178,17 @@
                 }
             }
             return buttonNumber;
+        }
+        function objClone(obj){
+            if (obj === null || typeof(obj) !=="object") return obj;
+            let copy = obj.constructor();
+
+            for(let attr in obj){
+                if(obj.hasOwnProperty(attr)){
+                    copy[attr] = objClone(obj[attr]);
+                }
+            }
+            return copy;
         }
         init();
     }
